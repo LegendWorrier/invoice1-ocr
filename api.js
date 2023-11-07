d8e953202d732a09002720448c93a96c837d7c40
LegendWorrier <148674491+LegendWorrier@users.noreply.github.com>
2023-11-07 10:41:14 +0900

    Update api.js

--- api.js
+++ api.js
@@ -76,72 +76,72 @@ app.use(bodyParser.json());
 //   html: '<p>This is the HTML version of the email.</p>',
 // };
 
-const server = http.createServer(app);
-var chatServer = require('http').Server(app);
+// const server = http.createServer(app);
+// var chatServer = require('http').Server(app);
 
 // const io = socketIO(server);
 
 // Create a Socket.IO server instance
 
 // Socket.IO event: client connected
-const io = require('socket.io')(chatServer, {
-  cors: {
-  }
-});
-var ns = io.of("/");
-io.on('connection', (socket) => {
-  console.log('aaaaa', process.env.NODE_ENV)
+// const io = require('socket.io')(chatServer, {
+//   cors: {
+//   }
+// });
+// var ns = io.of("/");
+// io.on('connection', (socket) => {
+//   console.log('aaaaa', process.env.NODE_ENV)
 
-  // transport.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //     console.error('Error sending email:', error);
-  //   } else {
-  //     console.log('Email sent:', info.response);
-  //   }
-  // });
+//   // transport.sendMail(mailOptions, (error, info) => {
+//   //   if (error) {
+//   //     console.error('Error sending email:', error);
+//   //   } else {
+//   //     console.log('Email sent:', info.response);
+//   //   }
+//   // });
 
-  console.log('A client connected', ns.connected);
+//   console.log('A client connected', ns.connected);
 
-  // Socket.IO event: client disconnected
-  socket.on('disconnect', () => {
-    console.log('A client disconnected');
+//   // Socket.IO event: client disconnected
+//   socket.on('disconnect', () => {
+//     console.log('A client disconnected');
 
-  });
-  socket.on('Add User', (ary) => {
-    // Note
-    console.log('ary,', ary)
-    console.log('Add User', ary)
-    let notification = new Note ({
-      user_id: ary.userid,
-      agency_id:ary.client,
-      msg_content: ary.user
-    })
-    notification
-    .save()
-    .then((data) => {
-      console.log('daaa', data);
-    socket.broadcast.emit('registerNewUsertoClient', data)
+//   });
+//   socket.on('Add User', (ary) => {
+//     // Note
+//     console.log('ary,', ary)
+//     console.log('Add User', ary)
+//     let notification = new Note ({
+//       user_id: ary.userid,
+//       agency_id:ary.client,
+//       msg_content: ary.user
+//     })
+//     notification
+//     .save()
+//     .then((data) => {
+//       console.log('daaa', data);
+//     socket.broadcast.emit('registerNewUsertoClient', data)
       
-    })
-    .catch((err) => {
-      console.log(err)});
-    var flag = false
-    console.log(ns.connected)
+//     })
+//     .catch((err) => {
+//       console.log(err)});
+//     var flag = false
+//     console.log(ns.connected)
 
-    //   if (ns) {
-    //     Object.keys(ns.connected).forEach(function (id) {
+//     //   if (ns) {
+//     //     Object.keys(ns.connected).forEach(function (id) {
 
-    //       if(ns.connected[id].userID == userID){
-    //         flag = true;
-    //       }    
-    //     });
-    // }
-    // if(!flag) {
-    //   socket.userID = userID;
-    // }
+//     //       if(ns.connected[id].userID == userID){
+//     //         flag = true;
+//     //       }    
+//     //     });
+//     // }
+//     // if(!flag) {
+//     //   socket.userID = userID;
+//     // }
 
-  })
-});
+//   })
+// });
 
 // app.post('/upload', upload.array('files'), (req, res) => {
   // Access the uploaded files via req.files
@@ -152,117 +152,117 @@ io.on('connection', (socket) => {
   // res.send('Files uploaded successfully');
 // });
 
-function isFilenameWithinDateRange(fileName, startDate, endDate) {
-  const datePart = fileName.split('-')[1].split('.')[0];
-  const fileDate = new Date(Number(datePart));
-  return fileDate >= startDate && fileDate <= endDate;
-}
+// function isFilenameWithinDateRange(fileName, startDate, endDate) {
+//   const datePart = fileName.split('-')[1].split('.')[0];
+//   const fileDate = new Date(Number(datePart));
+//   return fileDate >= startDate && fileDate <= endDate;
+// }
 
-app.get('/download', async (req, res) => {
+// app.get('/download', async (req, res) => {
 
-  const filesString = req.query.param3;
-  const files = filesString.split(',');
+//   const filesString = req.query.param3;
+//   const files = filesString.split(',');
 
-  console.log('download', files)
+//   console.log('download', files)
 
-  const uploadFolderPath = path.join(__dirname, 'uploads');
-  const prefixToMatch = req.query.param2
-  const startDate = new Date(req.query.param1);
-  const endDate = new Date();
+//   const uploadFolderPath = path.join(__dirname, 'uploads');
+//   const prefixToMatch = req.query.param2
+//   const startDate = new Date(req.query.param1);
+//   const endDate = new Date();
 
-  const mergedFileName = 'merged.pdf';
-  const mergedFilePath = path.join(path.join(__dirname, 'merge'), mergedFileName);
-  const regex = new RegExp(`^${prefixToMatch}-[0-9]+\\.[A-Za-z]+$`);
+//   const mergedFileName = 'merged.pdf';
+//   const mergedFilePath = path.join(path.join(__dirname, 'merge'), mergedFileName);
+//   const regex = new RegExp(`^${prefixToMatch}-[0-9]+\\.[A-Za-z]+$`);
 
-  // const fileNames = fs.readdirSync(uploadFolderPath);
-  // const pdfFiles = fileNames.filter((fileName) =>
-  // fileName.match(prefixToMatch) && fileName.toLowerCase().endsWith('.pdf') &&
-  //   isFilenameWithinDateRange(fileName, startDate, endDate)
-  // );
+//   // const fileNames = fs.readdirSync(uploadFolderPath);
+//   // const pdfFiles = fileNames.filter((fileName) =>
+//   // fileName.match(prefixToMatch) && fileName.toLowerCase().endsWith('.pdf') &&
+//   //   isFilenameWithinDateRange(fileName, startDate, endDate)
+//   // );
 
-  // console.log('pdfFiles', pdfFiles)
+//   // console.log('pdfFiles', pdfFiles)
 
-  const mergedPdf = await PDFDocument.create();
+//   const mergedPdf = await PDFDocument.create();
 
-  for (const pdfFile of files) {
-    const pdfBytes = fs.readFileSync(path.join(uploadFolderPath, pdfFile));
-    const pdf = await PDFDocument.load(pdfBytes);
-    const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
-    copiedPages.forEach((page) => mergedPdf.addPage(page));
-  }
-  const mergedPdfBytes = await mergedPdf.save();
-  fs.writeFileSync(mergedFilePath, mergedPdfBytes);
-  // res.setHeader('Content-Disposition', `attachment; filename="${mergedFileName}"`);
-  res.setHeader('Content-Type', 'application/pdf');
+//   for (const pdfFile of files) {
+//     const pdfBytes = fs.readFileSync(path.join(uploadFolderPath, pdfFile));
+//     const pdf = await PDFDocument.load(pdfBytes);
+//     const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+//     copiedPages.forEach((page) => mergedPdf.addPage(page));
+//   }
+//   const mergedPdfBytes = await mergedPdf.save();
+//   fs.writeFileSync(mergedFilePath, mergedPdfBytes);
+//   // res.setHeader('Content-Disposition', `attachment; filename="${mergedFileName}"`);
+//   res.setHeader('Content-Type', 'application/pdf');
 
-  // Send the merged PDF file as the response
-  res.sendFile(mergedFilePath);
+//   // Send the merged PDF file as the response
+//   res.sendFile(mergedFilePath);
 
 
 
-  // const filePath = path.join(__dirname, 'uploads', 'file-64e660293379f655c89b8afb-1695169308156-77933161.pdf');
-  // // const filePath = path.join(uploadFolderPath, );
-  // console.log('upoload', filePath)
+//   // const filePath = path.join(__dirname, 'uploads', 'file-64e660293379f655c89b8afb-1695169308156-77933161.pdf');
+//   // // const filePath = path.join(uploadFolderPath, );
+//   // console.log('upoload', filePath)
 
-  // // Check if the file exists 
+//   // // Check if the file exists 
 
-  // if (fs.existsSync(filePath)) {
+//   // if (fs.existsSync(filePath)) {
 
-  //   // Set the response headers
-  //   // res.setHeader('Content-Disposition', 'attachment; filename="document.pdf"');
-  //   // Create a read stream for the file
-  //   res.setHeader('Content-Type', 'application/bin');
+//   //   // Set the response headers
+//   //   // res.setHeader('Content-Disposition', 'attachment; filename="document.pdf"');
+//   //   // Create a read stream for the file
+//   //   res.setHeader('Content-Type', 'application/bin');
 
-  //   const fileStream = fs.createReadStream(filePath);
+//   //   const fileStream = fs.createReadStream(filePath);
 
-  //   // Pipe the file stream to the response
-  // // res.send(fileStream);
-  // // res.download(filePath, fileName);
-  // // res.sendFile(filePath);
-  //   fileStream.pipe(res);
-  // } else {
-  //   res.status(404).send('File not found');
-  // }
+//   //   // Pipe the file stream to the response
+//   // // res.send(fileStream);
+//   // // res.download(filePath, fileName);
+//   // // res.sendFile(filePath);
+//   //   fileStream.pipe(res);
+//   // } else {
+//   //   res.status(404).send('File not found');
+//   // }
 
-})
+// })
 
-app.get('/simpledownload', (req, res) => {
+// app.get('/simpledownload', (req, res) => {
 
-  console.log('upoload', req.query.param)
+//   console.log('upoload', req.query.param)
 
-  const filePath = path.join(__dirname, 'uploads', req.query.param);
-  // const filePath = path.join(uploadFolderPath, );
+//   const filePath = path.join(__dirname, 'uploads', req.query.param);
+//   // const filePath = path.join(uploadFolderPath, );
 
-  // Check if the file exists 
+//   // Check if the file exists 
 
-  if (fs.existsSync(filePath)) {
+//   if (fs.existsSync(filePath)) {
 
-    // Set the response headers
-    // res.setHeader('Content-Disposition', 'attachment; filename="document.pdf"');
-    // Create a read stream for the file
-    res.setHeader('Content-Type', 'application/bin');
+//     // Set the response headers
+//     // res.setHeader('Content-Disposition', 'attachment; filename="document.pdf"');
+//     // Create a read stream for the file
+//     res.setHeader('Content-Type', 'application/bin');
 
-    const fileStream = fs.createReadStream(filePath);
+//     const fileStream = fs.createReadStream(filePath);
 
-    // Pipe the file stream to the response
-    // res.send(fileStream);
-    // res.download(filePath, fileName);
-    res.sendFile(filePath);
-    // fileStream.pipe(res);
-  } else {
-    res.status(404).send('File not found');
-  }
+//     // Pipe the file stream to the response
+//     // res.send(fileStream);
+//     // res.download(filePath, fileName);
+//     res.sendFile(filePath);
+//     // fileStream.pipe(res);
+//   } else {
+//     res.status(404).send('File not found');
+//   }
 
-})
+// })
 
 // Create an array to hold the Sharp objects representing the images
 
 
 // Start the server
-const port1 = 8888; // Specify the port you want to use
-chatServer.listen(port1, () => {
-  console.log(`SocketServer listening on port ${port1}`);
-});
+// const port1 = 8888; // Specify the port you want to use
+// chatServer.listen(port1, () => {
+//   console.log(`SocketServer listening on port ${port1}`);
+// });
 
 
 
