c8042a8b39f20987cc946fde5f9d21a7858734d3
LegendWorrier <148674491+LegendWorrier@users.noreply.github.com>
2023-11-07 11:06:03 +0900

    Update api.js

--- api.js
+++ api.js
@@ -300,6 +300,7 @@ app.use(express.static('client/build'))
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
+module.exports = app
 // const port = process.env.PORT || 5000;
 
 // app.listen(port, () => console.log('Server running on port', port));
