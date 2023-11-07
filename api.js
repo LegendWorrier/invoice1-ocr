0b3e6f612ec9baf38b041f37a14fb24f59d969fb
LegendWorrier <148674491+LegendWorrier@users.noreply.github.com>
2023-11-07 10:57:48 +0900

    Update api.js

--- api.js
+++ api.js
@@ -300,6 +300,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Server running on port', port));
+// app.listen(port, () => console.log('Server running on port', port));
