b50c147666bf67976ca647af70b7588d7ad3cc9d
LegendWorrier <148674491+LegendWorrier@users.noreply.github.com>
2023-11-07 10:51:11 +0900

    Create keys_dev.js

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // mongoURI: 'mongodb://localhost/my_db',
+    mongoURI: 'mongodb+srv://wasi:tiger9999@cluster0.d18e063.mongodb.net/my_db',
+    secretOrKey: 'secret'
+  };
+  
