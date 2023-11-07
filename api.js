11b11fde8bcfb1b612b07b0cf1457332da5ccc7e
LegendWorrier <148674491+LegendWorrier@users.noreply.github.com>
2023-11-07 11:10:28 +0900

    Update api.js

--- api.js
+++ api.js
@@ -287,15 +287,15 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 app.use('/api/notifications', Notification);
 
-// app.use(express.static('client/build'))
+app.use(express.static('client/build'))
 
 // if (process.env.NODE_ENV === 'production') {
   //Set static folder
 
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  // app.use(express.static('client/build'))
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // })
 // }
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
